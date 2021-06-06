a1b87a2d7ae39143b9d3d8ddd41b41bd3ee33228
Salamdi <dinilam91@gmail.com>
2021-06-06 18:35:33 +0300

    add format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,10 @@ import { Command } from 'commander';
 const gendiff = new Command();
 
 gendiff.description('Compares two configuration files and shows a difference.');
-gendiff.version('1.0.0');
-gendiff.parse(process.argv);
+gendiff
+  .version('1.0.0')
+  .option('-f, --format', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .parse(process.argv);
 
 export default (filepath1, filepath2) => `${filepath1} & ${filepath2} diff`;
