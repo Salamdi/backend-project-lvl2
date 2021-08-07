b4173d7536342dd15af441275d3553f643d21cc1
Salamdi <dinislam91@gmail.com>
2021-08-07 16:18:38 +0300

    refactor gendiff

--- __tests__/gendiff.js
+++ __tests__/gendiff.js
@@ -1,29 +1,19 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import gendiff from '../src/index';
+import { readFileSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const file1 = join(__dirname, '..', '__fixtures__', 'file1.json');
 const file2 = join(__dirname, '..', '__fixtures__', 'file2.json');
+const expectedDiffPath = join(__dirname, '..', '__fixtures__', 'difference.json');
 
-describe('flat json comparison', () => {
+describe('nested objects comparison', () => {
+  const expectedDiff = JSON.parse(readFileSync(expectedDiffPath));
   const diff = gendiff(file1, file2);
 
-  test('correctly compares unchanged field', () => {
-    expect(diff).toContain('   host: hexlet.io');
-  });
-
-  test('correctly compares changed field', () => {
-    expect(diff).toContain(' - timeout: 50');
-    expect(diff).toContain(' + timeout: 20');
-  });
-
-  test('detects new field', () => {
-    expect(diff).toContain(' + verbose: true');
-  });
-
-  test('detects removed field', () => {
-    expect(diff).toContain(' - proxy: 123.234.53.22');
+  test('correctly calculates difference of nested objects', () => {
+    expect(diff).toEqual(expectedDiff);
   });
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,18 +1,30 @@
 import _ from 'lodash';
 
-export default (obj1, obj2) => {
+const gendiff = (obj1, obj2, depth = 0) => {
   const union = { ...obj1, ...obj2 };
-  const result = Object
-    .keys(union)
-    .reduce((res, key) => {
-      if (obj1[key] === obj2[key]) return `${res}   ${key}: ${union[key]}\n`;
-      if (!_.isUndefined(obj1[key]) && obj2[key] === undefined) return `${res} - ${key}: ${union[key]}\n`;
-      if (_.isUndefined(obj1[key]) && obj2[key] !== undefined) return `${res} + ${key}: ${union[key]}\n`;
-      if (obj1[key] !== obj2[key]) {
-        return `${res} - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}\n`;
-      }
-      return res;
-    }, '\n');
-
-  return `{${result}}`;
+  return _.reduce(union, (diff, value, key) => {
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      diff[key] = gendiff(obj1[key], obj2[key], depth + 1);
+      return diff;
+    }
+    if (obj1[key] === obj2[key]) {
+      diff[key] = value;
+      return diff;
+    }
+    if (!_.isUndefined(obj1[key]) && _.isUndefined(obj2[key])) {
+      diff[` - ${key}`] = obj1[key];
+      return diff;
+    }
+    if (_.isUndefined(obj1[key]) && !_.isUndefined(obj2[key])) {
+      diff[` + ${key}`] = obj2[key];
+      return diff;
+    }
+    if (obj1[key] !== obj2[key]) {
+      diff[` - ${key}`] = obj1[key];
+      diff[` + ${key}`] = obj2[key];
+      return diff;
+    }
+  }, {});
 };
+
+export default gendiff;
