c9c8a2ec7b29fa7f2152dcedc5fe7a81380d51a7
Salamdi <dinislam91@gmail.com>
2021-08-23 15:37:07 +0300

    refactor

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,49 +3,42 @@ import {
   REMOVED, ADDED, UPDATED, TYPE_KEY, NEW_KEY, OLD_KEY,
 } from './constants.js';
 
+const reduceProp = (prop1, prop2) => {
+  if (prop1 === prop2) return prop2;
+  if (!_.isUndefined(prop1) && _.isUndefined(prop2)) {
+    return {
+      [TYPE_KEY]: REMOVED,
+      [OLD_KEY]: prop1,
+    };
+  }
+  if (_.isUndefined(prop1) && !_.isUndefined(prop2)) {
+    return {
+      [TYPE_KEY]: ADDED,
+      [NEW_KEY]: prop2,
+    };
+  }
+  if (prop1 !== prop2) {
+    return {
+      [TYPE_KEY]: UPDATED,
+      [OLD_KEY]: prop1,
+      [NEW_KEY]: prop2,
+    };
+  }
+  return null;
+};
+
 const gendiff = (obj1, obj2, depth = 0) => {
   const keys = _.flow(
     _.concat,
     _.uniq,
     _.sortBy,
   )(_.keys(obj1), _.keys(obj2));
-  return _.reduce(keys, (diff, key) => {
-    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return { ...diff, [key]: gendiff(obj1[key], obj2[key], depth + 1) };
-    }
-    if (obj1[key] === obj2[key]) {
-      return { ...diff, [key]: obj2[key] };
-    }
-    if (!_.isUndefined(obj1[key]) && _.isUndefined(obj2[key])) {
-      return {
-        ...diff,
-        [key]: {
-          [TYPE_KEY]: REMOVED,
-          [OLD_KEY]: obj1[key],
-        },
-      };
-    }
-    if (_.isUndefined(obj1[key]) && !_.isUndefined(obj2[key])) {
-      return {
-        ...diff,
-        [key]: {
-          [TYPE_KEY]: ADDED,
-          [NEW_KEY]: obj2[key],
-        },
-      };
-    }
-    if (obj1[key] !== obj2[key]) {
-      return {
-        ...diff,
-        [key]: {
-          [TYPE_KEY]: UPDATED,
-          [OLD_KEY]: obj1[key],
-          [NEW_KEY]: obj2[key],
-        },
-      };
-    }
-    return diff;
-  }, {});
+  return _.reduce(keys, (diff, key) => ({
+    ...diff,
+    [key]: (_.isObject(obj1[key]) && _.isObject(obj2[key]))
+      ? gendiff(obj1[key], obj2[key], key + 1)
+      : reduceProp(obj1[key], obj2[key], depth),
+  }), {});
 };
 
 export default gendiff;
